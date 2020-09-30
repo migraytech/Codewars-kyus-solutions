81d09344a8dcca65abc2f7b99ca545bed94d1d59
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2020-09-30 23:52:54 +0200

    Create Ransom.java

---
+++ 6-kyu/6kyu-solutions/src/main/java/Ransom.java
@@ -0,0 +1,13 @@
+public class Ransom {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean canMakeNote(String note, String magazine) {
+
+
+       return false;
+
+    }
+}
