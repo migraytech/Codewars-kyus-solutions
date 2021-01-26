01c42263aace73858a11cfa9c38ae41c9c56b18e
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-01-26 22:55:36 +0100

    Create VowelCount.java

---
+++ 7-kyu/7kyu-solutions/src/main/java/VowelCount.java
@@ -0,0 +1,28 @@
+public class VowelCount {
+
+    public static void main(String[] args) {
+
+
+        String word = "abracadabra";
+        getCount(word);
+    }
+
+    public static int getCount(String word)
+    {
+       int vowels = 0;
+
+
+       for (int i=0; i<word.length(); i++)
+        {
+            char character = word.charAt(i);
+            if(character == 'a'||character == 'e'||character == 'i'||character == 'u'||character == 'o')
+                ++vowels;
+        }
+
+        return vowels;
+    }
+
+
+
+
+}
