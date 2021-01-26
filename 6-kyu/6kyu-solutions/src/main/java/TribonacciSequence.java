83ca6679c96f80c38e6f4d1139167d09cf84b3c5
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-01-26 22:17:26 +0100

    Update TribonacciSequence.java

--- 6-kyu/6kyu-solutions/src/main/java/TribonacciSequence.java
+++ 6-kyu/6kyu-solutions/src/main/java/TribonacciSequence.java
@@ -1,12 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TribonacciSequence {
 
     public static void main(String[] args) {
 
+        int n = 10;
+        double [] test =  new double [] {1,1,1};
+        double [] test2 =  new double [] {0,1,1};
+        double [] test3 =  new double [] {0,0,1};
 
-        tribonacci(double[] s, int n
+        System.out.print(Arrays.toString(tribonacci(test,n)));
     }
 
 
 
-    public static double []
+    public static double [] tribonacci(double[] s, int n )
+    {
+        // each index bij elkaar op tellen
+        // van de vorige value
+        // kijk naar aantal keer bij 10 length
+        double [] newSquences = new double[0];
+        for (int i=0; i<n; i++)
+        {
+            newSquences = s;
+            
+
+        }
+        return newSquences;
+    }
 }
