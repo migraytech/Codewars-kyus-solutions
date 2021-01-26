49bbc2dd66a76ecee988b7139a8120d2376e9bb1
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-01-26 12:51:49 +0100

    Create UniqueNumber.java

---
+++ 6-kyu/6kyu-solutions/src/main/java/UniqueNumber.java
@@ -0,0 +1,16 @@
+public class UniqueNumber {
+
+
+    //Find the unique numbers
+    public static void main(String[] args) {
+
+    }
+
+
+    
+    public static double findUniq(double arr[]) {
+        // Do the magic
+        return arr[0];
+    }
+
+}
