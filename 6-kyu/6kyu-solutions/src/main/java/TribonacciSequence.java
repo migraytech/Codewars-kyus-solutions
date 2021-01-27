079bb68cc234295cbfad657a57498821923aeb11
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-01-27 01:19:48 +0100

    Update TribonacciSequence.java

--- 6-kyu/6kyu-solutions/src/main/java/TribonacciSequence.java
+++ 6-kyu/6kyu-solutions/src/main/java/TribonacciSequence.java
@@ -5,37 +5,50 @@ public class TribonacciSequence {
 
     public static void main(String[] args) {
 
-        int n = 10;
-        double [] test =  new double [] {1,1,1};
+        int n = 2;
+        double [] test =  new double []{9.0, 8.0, 17.0};
         double [] test2 =  new double [] {0,1,1};
         double [] test3 =  new double [] {0,0,1};
+        double [] test4 = new double[] {8.0, 4.0, 16.0};
 
         System.out.print(Arrays.toString(tribonacci(test,n)));
-       System.out.print(fib(10));
     }
 
 
 
-    public static double [] tribonacci(double[] s, int n )
-    {
-        // each index bij elkaar op tellen
-        // van de vorige value
-        // kijk naar aantal keer bij 10 length
-        //math om elk 3 verplaatsen en op te tellen.
+    // Tribonacci  squences werkt
+    // {0,0,1}
+    // {1,1,1}
+    // {0,1,1}
+    public static double[] tribonacci(double[] s,int n) {
+        double[] x;
+        int t = 0;
+        if(n == 0) return new double[0];
 
-        //start
-        int previous = 0;
-        double [] newSquences = s;
+        if (n <= s.length)
+                {
+            x = new double[n];
 
+            for(int i = 0; i < n; i++){
+                x[i] = s[i];
+            }
 
+            return x;
+       }
+        else
+            x = new double[n];
 
-        return newSquences;
+        //fill the value of the given array
+        for (double test:s) {
+            x[t] = test;
+            t++;
+        }
+
+        // set in dp of fill x array values
+        double dp []= x;
+        for (int i = 3; i < n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+
+        return dp;
     }
-    static int fib(int n)
-    {
-        if (n <= 1)
-            return n;
-        return fib(n-1) + fib(n-2) +fib(n-3);
-    }
-
 }
