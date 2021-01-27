88c91e5d6d5003bb5acb73a799db62dace45e6de
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-01-27 15:34:01 +0100

    Update RemoveDuplicates.java

--- 7-kyu/7kyu-solutions/src/main/java/RemoveDuplicates.java
+++ 7-kyu/7kyu-solutions/src/main/java/RemoveDuplicates.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
@@ -9,26 +9,35 @@ public class RemoveDuplicates {
         int [] test3 = new int[]{1,2,3,4};
         int [] test4 = new int[]{1,1,4,5,1,2,1};
 
-        System.out.print(Arrays.toString(solve(test)));
+        System.out.print(Arrays.toString(solve(test2)));
     }
 
-    public static int[] solve (int [] array)
+    public static int[] solve (int [] arr)
     {
         int [] newarray = new int[0];
+        ArrayList<Integer> checkerList = new ArrayList<>();
+        List<Integer> list = new ArrayList<>(arr.length);
 
-        for (int i = 0; i <array.length; i++) {
-            for (int j = array.length-1; j>0;  j--) {
-
-                if(array [i] == array[j])
-                {
-                    if( i != j)
-                    {
-                        newarray[i] = array[i];
-                    }
-                }
-            }
+        for (int i : arr) {
+            list.add(i);
         }
 
-        return newarray;
+        // get first index copy
+         for (int i = 0;  i<arr.length;  i++) {
+            for (int x = i+1; x < arr.length ; x++) {
+                if (arr[i] == arr[x]) {
+                    if(!checkerList.contains(i))
+                        checkerList.add(i);
+                }
+            }
+         }
+
+        // remove first left duplicates
+        for (Integer integer : checkerList) {
+            list.remove(list.get(integer));
+        }
+
+
+        return list.parallelStream().mapToInt(Integer::intValue).toArray();
     }
 }
