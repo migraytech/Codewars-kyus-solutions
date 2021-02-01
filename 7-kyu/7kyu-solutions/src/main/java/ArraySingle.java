cb572b40a3556d92183adde94ccb1f6102fae986
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-02-01 13:22:24 +0100

    Create ArraySingle.java

---
+++ 7-kyu/7kyu-solutions/src/main/java/ArraySingle.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class ArraySingle {
+
+    public static void main(String[] args) {
+
+        int array [] = new int[] {0,0,3,4,6,3};
+    }
+
+    public static int repeats(int [] arr){
+
+        HashMap<Integer, Integer>  counterList = new HashMap<>();
+        int counter = 0;
+        for (int value : arr) {
+            if(counterList.containsKey(value)){
+                counter++;
+                counterList.
+            }
+            else
+            {
+                int count = counterList.get(value);
+                cot
+            }
+        }
+
+
+    }
+}
