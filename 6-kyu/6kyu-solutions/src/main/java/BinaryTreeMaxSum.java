e06b34b721935e770e16ac3339ce1028e5b2d59d
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-02-01 22:44:30 +0100

    new case

--- 6-kyu/6kyu-solutions/src/main/java/BinaryTreeMaxSum.java
+++ 6-kyu/6kyu-solutions/src/main/java/BinaryTreeMaxSum.java
@@ -1,9 +1,81 @@
 public class BinaryTreeMaxSum {
 
     public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        /*create root*/
+        tree.root = new TreeNode(1);
+
+        /* following is the tree after above statement
+
+              1
+            /   \
+          null  null     */
+
+        tree.root.left = new TreeNode(2);
+        tree.root.right = new TreeNode(3);
+
+        /* 2 and 3 become left and right children of 1
+               1
+            /     \
+          2        3
+        /   \     /  \
+      null null null null  */
+
+
+        tree.root.left.left = new TreeNode(4);
+        /* 4 becomes left child of 2
+                    1
+                /       \
+               2          3
+             /   \       /  \
+            4    null  null  null
+           /   \
+          null null
+         */
+        System.out.print(maxSum(tree.root));
+    }
+
+    public static int maxSum(TreeNode node)
+    {
+        // the sum of all elements in a binary tree.
+
+        if(node == null)
+            return 0;
+        else
+        {
+            return node.key+ maxSum(node.left)+ maxSum(node.right);
+        }
 
     }
 
 
-    
+    public static class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int key;
+
+        public TreeNode(int item)
+        {
+            key = item;
+            left = right = null;
+        }
+    }
+
+    public static class BinaryTree
+    {
+        TreeNode root;
+
+        // Constructors
+        BinaryTree(int key)
+        {
+            root = new TreeNode(key);
+        }
+
+        BinaryTree()
+        {
+            root = null;
+        }
+    }
+
 }
---
+++ 6-kyu/6kyu-solutions/src/main/java/Multiples.java
@@ -0,0 +1,38 @@
+public class Multiples {
+
+    public static void main(String[] args) {
+
+        int value = 20;
+        System.out.print(solution(value));
+
+
+    }
+
+    public static int solution(int number) {
+
+        // op sommen onder het getal van 10
+        // multiples  x =5 , y = 3
+        int y = 3;
+        int x = 5;
+        int solution = 0;
+        if( number < 0) {
+            return 0;
+        }
+        else{
+
+            for( int i = 0; i < number; i++) {
+                 int sumY = (i*y);
+                 int sumX = (i*x);
+
+                if(i %3 == 0 || i  %5 == 0)  //check the number is divisible by 3 or 5
+                {
+                    solution += i;
+
+                }
+            }
+        }
+
+        return solution;
+    }
+
+}
