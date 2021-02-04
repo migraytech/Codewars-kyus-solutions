962afb56e733e357f75abb863e04250313743005
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-02-05 00:12:05 +0100

    Update Pincode.java

--- 7-kyu/7kyu-solutions/src/main/java/Pincode.java
+++ 7-kyu/7kyu-solutions/src/main/java/Pincode.java
@@ -1,31 +1,22 @@
 public class Pincode {
 
     public static void main(String[] args) {
-
-
-        String pincode = "090909";
-        String pincode2 = "123456";
-        String pincode3 = ".234";
-        String pincode4 = "a.234";
-                                                                                                                                                       ";
-
-
-
-        System.out.print(validatePin());
-
+        
     }
 
 
 
     public static boolean validatePin(String pin) {
 
-        String newvalue = pin.trim().replaceAll("l", "");
+        String newvalue = pin.trim();
         int valideLength = 4;
         int valideLength2 = 6;
+
+        if(pin.contains("\n"))
+            return false;
         if (newvalue.length() == valideLength ||  newvalue.length() == valideLength2 ) {
             return newvalue.trim().matches("[0-9]+");
         }
-
 
         return false;
 
