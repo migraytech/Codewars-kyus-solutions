974186027e8e489a3fa759e3230b6890e1af0e8a
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-02-05 22:43:13 +0100

    new 5kyu solution

---
+++ 5-kyu/5kyu-solutions/src/main/java/FunOfTrees.java
@@ -0,0 +1,16 @@
+import javax.swing.tree.TreeNode;
+
+public class FunOfTrees {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static TreeNode arrayToTree(int[] array) {
+
+        return null; // TODO: implementation
+    }
+
+}
