09c73234c7b621373a13e23a142eb6b138b6351e
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-02-06 14:45:35 +0100

    Update IsPerfection.java

--- 5-kyu/5kyu-solutions/src/main/java/IsPerfection.java
+++ 5-kyu/5kyu-solutions/src/main/java/IsPerfection.java
@@ -1,5 +1,37 @@
 public class IsPerfection {
     public static void main(String[] args) {
-        
+
+    }
+
+
+    public static boolean isPerfect(FunOfTrees.TreeNode root) {
+
+        if( root == null) {
+            return true;
+        }
+
+        // check fot the full unbalanced tree for the left and right side
+        if(countChildren(root.left) != countChildren(root.right))
+            return false;
+
+        if( root.left == null && root.right == null) {
+            return true;
+        }
+        else {
+            if(root.left == null || root.right == null)
+                return false;
+            return isPerfect(root.left) || isPerfect(root.right);
+        }
+
+    }
+
+    public static int countChildren(FunOfTrees.TreeNode root)
+    {
+        if(root == null)
+            return 0;
+        else
+        {
+            return 1+countChildren(root.left) + countChildren(root.right);
+        }
     }
 }
