d39ba49807f3d3c9ca0779a31467c54f4940f51a
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-02-07 13:03:16 +0100

    new kyu

---
+++ 6-kyu/6kyu-solutions/src/main/java/ConsecutiveStrings.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsecutiveStrings {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static String longestConsec(String[] strarr, int k) {
+
+        String solution = "";
+        List<String> listOfLongest =  new ArrayList<String>();
+
+
+     
+
+
+
+
+        return "";
+    }
+}
