b2992cf4614b97342f7ac2d02ef72ca2bfc571c2
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-09-23 12:38:12 +0200

    Update Snail.java

--- 4-kyu/4kyu-solutions/src/main/java/Snail.java
+++ 4-kyu/4kyu-solutions/src/main/java/Snail.java
@@ -1,11 +1,38 @@
+import java.util.Arrays;
+
 public class Snail {
 
     public static void main(String[] args) {
+        int[][] array
+                = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
 
+        System.out.print(Arrays.toString(snail(array)));
     }
 
 
     public static int[] snail(int[][] array) {
-        // enjoy
+
+        int solution [] = new int[0];
+        int bottomLeft = 0;
+        int Upright = 0;
+
+        // copy the array
+        int [][] copyArray =  array;
+        for (int i =0; i< array.length-1; i++)
+        {
+
+            // first length-1 of first row
+
+            //
+        }
+
+
+        private static void String
+
+
+
+        return solution ;
     }
 }
