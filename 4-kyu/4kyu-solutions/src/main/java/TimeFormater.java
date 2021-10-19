c5e52ccab7bbb76a9d1b37709df01f3b3701736a
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-10-19 16:06:00 +0200

    Create TimeFormater.java

---
+++ 4-kyu/4kyu-solutions/src/main/java/TimeFormater.java
@@ -0,0 +1,34 @@
+public class TimeFormater {
+
+
+    private static String result;
+
+//    assertEquals("1 second", TimeFormatter.formatDuration(1));
+//    assertEquals("1 minute and 2 seconds", TimeFormatter.formatDuration(62));
+//    assertEquals("2 minutes", TimeFormatter.formatDuration(120));
+//    assertEquals("1 hour", TimeFormatter.formatDuration(3600));
+//    assertEquals("1 hour, 1 minute and 2 seconds", TimeFormatter.formatDuration(3662));
+
+    public static void main(String[] args) {
+        int
+        result = formatDuration()
+
+
+    }
+
+
+
+
+
+    public static String formatDuration(int seconds) {
+        // your code goes here
+
+        return "";
+    }
+
+
+}
+
+
+
+
