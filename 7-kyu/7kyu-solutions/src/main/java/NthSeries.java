7c803ce52e271d66c7e252b3f5bad7c9742a40b8
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-10-20 10:13:22 +0200

    done

---
+++ 7-kyu/7kyu-solutions/src/main/java/NthSeries.java
@@ -0,0 +1,22 @@
+public class NthSeries {
+
+
+
+    //Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+    
+    //assertEquals("1.57", NthSeries.seriesSum(5));
+    //assertEquals("1.77", NthSeries.seriesSum(9));
+    //assertEquals("1.94", NthSeries.seriesSum(15));
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static String seriesSum(int n) {
+        // Happy Coding ^_^
+
+
+        return "";
+    }
+}
