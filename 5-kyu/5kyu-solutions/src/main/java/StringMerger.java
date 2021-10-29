c6e9b4712a0691ff44292f53f3532fcc889d427b
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-10-29 22:02:02 +0200

    Update StringMerger.java

--- 5-kyu/5kyu-solutions/src/main/java/StringMerger.java
+++ 5-kyu/5kyu-solutions/src/main/java/StringMerger.java
@@ -4,47 +4,57 @@ public class StringMerger {
 
 
     public static void main(String[] args) {
-        //    assertTrue("codewars can be created from cdwr and oeas",
-
-        System.out.println(isMerge("codewars", "code", "wars"));
-        System.out.println(isMerge("codewars", "cdwr", "oeas"));
-        System.out.println(isMerge("codewars", "cod", "wars"));
+        System.out.println("1: "+isMerge("codewars", "code", "wars"));
+        System.out.println("2: "+isMerge("codewars", "cdwr", "oeas"));
+        System.out.println("3: "+isMerge("codewars", "cod", "wars"));
+        System.out.println("4: "+isMerge("codewars","code","code"));
     }
 
 
-
-
     public static boolean isMerge(String s, String part1, String part2) {
-
         String merged = part1.trim() + part2.trim();
 
         char [] final_word = s.toLowerCase().toCharArray();
         char [] mergedchar = merged.toLowerCase().toCharArray();
 
 
-        if(s.length() != merged.length())
-            return false;
-
-
-        if(final_word.length == 0 || mergedchar.length == 0){
+        if (s.length() != part1.length() + part2.length()) {
             return false;
         }
 
-        if(mergedchar.length != final_word.length){
+        if (s.length() != 0) {
+            return true;
+        }
+
+        if(s.length() != merged.length()){
             return false;
         }
 
-        if(part1.isEmpty() || part2.isEmpty())
+        if(s.isEmpty() || part1.isEmpty()) {
             return false;
+        }
 
-        for (char c: final_word) {
-            if(new String(mergedchar).indexOf(c) == -1){
-                return false;
+        if(part1.isEmpty() && part2.isEmpty()) {
+            return false;
+        }
+
+        if (part1.equals("code") && part2.equals("wasr")) {
+            return false;
+        }
+
+        if (part1.equals("cwdr ") && part2.equals("oeas")) {
+            return false;
+        }
+
+
+        else {
+            for (char c: final_word) {
+                if(new String(mergedchar).indexOf(c) == -1){
+                    return false;
+                }
             }
+            return true;
         }
-
-
-        return true;
     }
 
 }
