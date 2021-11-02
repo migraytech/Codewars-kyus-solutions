f386e55986772b8b64ef218f6cfda9dad2c47aab
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-11-02 16:18:52 +0100

    Create FindTheMissingLetter.java

---
+++ 6-kyu/6kyu-solutions/src/main/java/FindTheMissingLetter.java
@@ -0,0 +1,20 @@
+public class FindTheMissingLetter {
+
+    public static void main(String[] args) {
+
+        assertEquals('e', Kata.findMissingLetter(new char[] { 'a','b','c','d','f' }));
+        assertEquals('P', Kata.findMissingLetter(new char[] { 'O','Q','R','S' }));
+    }
+
+    public static char findMissingLetter(char[] array)
+    {
+        return ' ';
+    }
+    //to lower case
+
+    //set the string of alphbat
+
+    // compare o list if contains is the same
+    // get the index
+    // check in for missing word
+}
