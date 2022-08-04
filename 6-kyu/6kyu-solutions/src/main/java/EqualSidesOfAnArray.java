6734634bdb047c4122c44c99c2a83bd495cc110f
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2022-08-04 17:06:41 +0200

    Create EqualSidesOfAnArray.java

---
+++ 6-kyu/6kyu-solutions/src/main/java/EqualSidesOfAnArray.java
@@ -0,0 +1,17 @@
+public class EqualSidesOfAnArray {
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    
+    public static int findEvenIndex(int[] arr) {
+
+        if(arr == null)
+            return -1;
+
+    }
+}
